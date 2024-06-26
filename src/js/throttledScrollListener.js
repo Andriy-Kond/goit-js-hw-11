98a24be4f83c00c7c0f9db6765935bc70a12b9f8
Andrii <andrii.kondratovich@gmail.com>
2024-06-26 15:11:16 +0300

    add: lodash throttle

--- src/js/throttledScrollListener.js
+++ src/js/throttledScrollListener.js
@@ -2,8 +2,13 @@ import customThrottle from './customThrottle';
 import scrollListener from './scrollListener';
 import { THROTTLE_DELAY } from './variables';
 
+const lodashThrottle = require('lodash.throttle');
+
 // Роблю власний throttle, бо lodash дико глючить:
-const throttledScrollListener = customThrottle(scrollListener, THROTTLE_DELAY);
+// const throttledScrollListener = customThrottle(scrollListener, THROTTLE_DELAY);
+
+const throttledScrollListener = lodashThrottle(scrollListener, THROTTLE_DELAY);
+
 // Підключаю lodash.debounce
 // const throttle = require('lodash.throttle'); // перший варіант підключення
 // const _ = require('lodash'); // другий варіант підключення - всю бібліотеку разом
